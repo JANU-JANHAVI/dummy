7a4dd6f181317fb7613aaf99674b3bd213d8bf36
JANU-JANHAVI <84670122+JANU-JANHAVI@users.noreply.github.com>
2022-08-09 19:45:44 +0530

    Update products.component.ts

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductDataService } from '../services/product-data.service';
+import { Product } from './product';
+
 
 @Component({
-  selector: 'app-products',
+  selector: 'app-product-list',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent implements OnInit {
-  name: string = 'hello world';
-  constructor() {}
+export class ProductListComponent implements OnInit {
+  products : Array<Product>;
 
-  ngOnInit(): void {}
-}
+  constructor(private product: ProductDataService) {}
+
+
+  ngOnInit(): void {
+    this.product.getProducts().subscribe((data: any[]) => {
+      // const { products } : any = data ; destruction syntax
+      this.products = (data as any).products;   //converting object into array of products
+      console.log(this.products);
+      console.log(data);
+    });
+  }
