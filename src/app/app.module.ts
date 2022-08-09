9bcd77dc63a28befbd619029b865f71252827ffd
JANU-JANHAVI <84670122+JANU-JANHAVI@users.noreply.github.com>
2022-08-09 19:43:39 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { ProductListComponent } from './products/products.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+// import { MaterialModule } from './core/material.module';
+
+// Material Modules
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { ProductsComponent } from './products/products.component';
 
-import { MatCardModule } from '@angular/material/card';
+
+
 @NgModule({
-  declarations: [AppComponent, ProductsComponent],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatIconModule,
+    
+    MatGridListModule,
     MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
-  providers: [],
+  declarations: [AppComponent, HelloComponent, ProductListComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
